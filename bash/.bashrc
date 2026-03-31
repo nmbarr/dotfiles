25de8019e1e863cbfef0a8e1a2a0fc6bbbb1332b
Nicholas Barr <nickbarr10@gmail.com>
2026-03-31 12:13:21 -0700

    Added clipboard install to install.sh and added exports for nvm/node

--- bash/.bashrc
+++ bash/.bashrc
@@ -53,3 +53,7 @@ export PATH="/opt/nvim-linux-$(uname -m)/bin:$PATH"
 
 # Prompt
 eval "$(oh-my-posh init bash --config ~/.config/ohmyposh/catppuccin.omp.json)"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- install.sh
+++ install.sh
@@ -23,6 +23,18 @@ sudo apt update && sudo apt install -y \
     ripgrep \
     fd-find
 
+# Clipboard provider
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    echo "Wayland detected, installing wl-clipboard..."
+    sudo apt install -y wl-clipboard
+elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+    echo "X11 detected, installing xclip..."
+    sudo apt install -y xclip
+else
+    echo "Could not detect display server, installing both..."
+    sudo apt install -y wl-clipboard xclip
+fi
+
 # Go
 GO_VERSION="1.24.1"
 curl -OL https://go.dev/dl/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
