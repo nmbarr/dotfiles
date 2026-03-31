c7214ae5493881d891d64b37643ccfdfba752fd2
Nicholas Barr <nickbarr10@gmail.com>
2026-03-31 13:40:04 -0700

    Remapped exit to normal mode from Ctrl+C to Ctrl+E

--- nvim/.config/nvim/lua/brick/remap.lua
+++ nvim/.config/nvim/lua/brick/remap.lua
@@ -1,84 +1,59 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.g.mapleader = " " -- space as leader
 
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true })
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { silent = true })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { silent = true })
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { silent = true })
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { silent = true })
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { silent = true })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { silent = true })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- open netrw
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- LSP navigation
+vim.keymap.set("n", "gd", vim.lsp.buf.definition,     { silent = true }) -- go to definition
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration,    { silent = true }) -- go to declaration
+vim.keymap.set("n", "gr", vim.lsp.buf.references,     { silent = true }) -- list references
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { silent = true }) -- go to implementation
+vim.keymap.set("n", "K",  vim.lsp.buf.hover,          { silent = true }) -- hover docs
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,      { silent = true }) -- rename symbol
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { silent = true }) -- code actions
 
-vim.api.nvim_set_keymap("n", "<leader>tf", "<Plug>PlenaryTestFile", { noremap = false, silent = false })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move selection down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move selection up
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "=ap", "ma=ap'a")
-vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
-
-vim.keymap.set("n", "<leader>lt", function()
-    vim.cmd [[ PlenaryBustedFile % ]]
-end)
+vim.keymap.set("n", "J",     "mzJ`z")     -- join lines, preserve cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")  -- scroll down, center
+vim.keymap.set("n", "<C-u>", "<C-u>zz")  -- scroll up, center
+vim.keymap.set("n", "n",     "nzzzv")    -- next result, center
+vim.keymap.set("n", "N",     "Nzzzv")    -- prev result, center
+vim.keymap.set("n", "=ap",   "ma=ap'a")  -- format paragraph, restore cursor
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]]) -- paste over selection without losing register
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- yank to system clipboard
+vim.keymap.set("n",           "<leader>Y", [["+Y]]) -- yank line to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")   -- delete without yanking
 
-vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+vim.keymap.set("i", "<C-e>", "<Esc>") -- Ctrl+e = Escape in insert mode
+vim.keymap.set("n", "Q", "<nop>")     -- disable ex mode
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")         -- open sessionizer in new window
+vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux-sessionizer -s 0 --vsplit<CR>")     -- sessionizer vsplit
+vim.keymap.set("n", "<M-H>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")    -- sessionizer new window
 
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux-sessionizer -s 0 --vsplit<CR>")
-vim.keymap.set("n", "<M-H>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
+vim.keymap.set("n", "<C-k>",     "<cmd>cnext<CR>zz") -- next quickfix item
+vim.keymap.set("n", "<C-j>",     "<cmd>cprev<CR>zz") -- prev quickfix item
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz") -- next loclist item
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz") -- prev loclist item
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- search/replace word under cursor
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })               -- make file executable
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
-
-vim.keymap.set(
-    "n",
-    "<leader>ea",
-    "oassert.NoError(err, \"\")<Esc>F\";a"
-)
-
-vim.keymap.set(
-    "n",
-    "<leader>ef",
-    "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj"
-)
-
-vim.keymap.set(
-    "n",
-    "<leader>el",
-    "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
-)
+-- Go error snippets
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")                                   -- return err
+vim.keymap.set("n", "<leader>ea", "oassert.NoError(err, \"\")<Esc>F\";a")                                         -- assert.NoError
+vim.keymap.set("n", "<leader>ef", "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj")  -- log.Fatalf
+vim.keymap.set("n", "<leader>el", "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i") -- logger.Error
 
 vim.keymap.set("n", "<leader>ca", function()
-    require("cellular-automaton").start_animation("make_it_rain")
+    require("cellular-automaton").start_animation("make_it_rain") -- cellular automaton rain
 end)
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+    vim.cmd("so") -- source current file
 end)
