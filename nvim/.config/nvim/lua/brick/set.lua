f3a8fe28c54aa1a1944d6228b50541d0adbcfee5
Nicholas Barr <nickbarr10@gmail.com>
2026-03-31 13:32:32 -0700

    Added comments to set file

--- nvim/.config/nvim/lua/brick/set.lua
+++ nvim/.config/nvim/lua/brick/set.lua
@@ -1,33 +1,41 @@
+-- Use block cursor always (disables GUI cursor shape changes)
 vim.opt.guicursor = ""
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+-- Line numbers
+vim.opt.nu = true               -- show absolute line number on current line
+vim.opt.relativenumber = true   -- show relative line numbers for all other lines
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- Indentation
+vim.opt.tabstop = 4             -- a tab character counts as 4 spaces
+vim.opt.softtabstop = 4        -- tab key inserts 4 spaces
+vim.opt.shiftwidth = 4         -- >> and << shift by 4 spaces
+vim.opt.expandtab = true       -- insert spaces instead of tab characters
+vim.opt.smartindent = true     -- auto-indent new lines based on syntax
 
-vim.opt.smartindent = true
+-- Display
+vim.opt.wrap = false           -- don't wrap long lines
+vim.opt.colorcolumn = ""       -- no column ruler/highlight
 
-vim.opt.wrap = false
+-- File safety
+vim.opt.swapfile = false       -- don't create swap files
+vim.opt.backup = false         -- don't create backup files
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"  -- persistent undo directory
+vim.opt.undofile = true        -- enable persistent undo across sessions
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+-- Search
+vim.opt.hlsearch = false       -- don't highlight all search matches
+vim.opt.incsearch = true       -- highlight matches as you type
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- UI
+vim.opt.termguicolors = true   -- enable 24-bit RGB colors
+vim.opt.scrolloff = 8          -- keep 8 lines visible above/below cursor
+vim.opt.signcolumn = "yes"     -- always show the sign column (prevents layout shift)
 
-vim.opt.termguicolors = true
+-- Filename parsing
+vim.opt.isfname:append("@-@")  -- allow @ in filenames (useful for gf / path resolution)
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+-- Performance
+vim.opt.updatetime = 50        -- faster CursorHold events (ms); improves LSP responsiveness
 
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = ""
-
-vim.diagnostic.config({ virtual_text = true })
+-- Diagnostics
+vim.diagnostic.config({ virtual_text = true })  -- show inline diagnostic messages
